cd91623456c49d528ff1d8f913c8c743e0efaa51
taapires <telmo.pires@gmail.com>
2013-07-11 10:54:34 +0000

    read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var indexFile = fs.readFileSync('index.html');
+var buffer = new Buffer(indexFile);
+var indexWrite = buffer.toString('utf-8');
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(indexWrite);
 });
 
 var port = process.env.PORT || 5000;
