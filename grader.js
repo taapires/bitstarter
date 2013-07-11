1d4d02a4fd8125cb22a8a8b02cc431018c875e4e
taapires <telmo.pires@gmail.com>
2013-07-11 21:26:52 +0000

    fix grader.js bugs

--- grader.js
+++ grader.js
@@ -1,24 +1,4 @@
-utomatically grade files for the presence of specified HTML tags/attributes.
-Uses commander.js and cheerio. Teaches command line application development
-and basic DOM parsing.
-
-References:
-
- + cheerio
-   - https://github.com/MatthewMueller/cheerio
-   - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
-   - http://maxogden.com/scraping-with-node.html
-
- + commander.js
-   - https://github.com/visionmedia/commander.js
-   - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
-
- + JSON
-   - http://en.wikipedia.org/wiki/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
-*/
-
+#!/usr/bin/env node
 var fs = require('fs');
 var rest = require('restler');
 var util = require('util');
